c63d1ed739f75b57071b91be07d7d4c8cee50132
manjunath <manjunath.mt0@gmail.com>
2023-11-10 18:36:28 +0530

    minor updates

--- plugin-settings/src/SyncPage.jsx
+++ plugin-settings/src/SyncPage.jsx
@@ -37,14 +37,20 @@ const SyncPage = (props) => {
       </div>
 
       <div className="cd-sync-animation-container">
-        <img src="https://cdn.cdsn.me/v1/storage/buckets/63e727f01c9cd2a8490f/files/6538845b8254f97b3eca/view?project=63e727dfc53e6679e400" />
+        <img
+          alt="CodeDesign Logo"
+          src="https://cdn.cdsn.me/v1/storage/buckets/63e727f01c9cd2a8490f/files/6538845b8254f97b3eca/view?project=63e727dfc53e6679e400"
+        />
         <video autoPlay loop muted>
           <source
             src="https://cdn.cdsn.me/v1/storage/buckets/63e727f01c9cd2a8490f/files/653884fe3107284184a3/view?project=63e727dfc53e6679e400"
             type="video/webm"
           />
         </video>
-        <img src="https://cdn.cdsn.me/v1/storage/buckets/63e727f01c9cd2a8490f/files/65388463d17b49210d47/view?project=63e727dfc53e6679e400" />
+        <img
+          alt="WordPress Logo"
+          src="https://cdn.cdsn.me/v1/storage/buckets/63e727f01c9cd2a8490f/files/65388463d17b49210d47/view?project=63e727dfc53e6679e400"
+        />
       </div>
       <div className="cd-sync-footer-container">
         <div className="cd-sync-footer-last-sync-and-info">
