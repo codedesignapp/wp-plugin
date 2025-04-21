00fc2d68a64e8ea5395d8c025b82dd1dfec5d269
GitHub Action <support@codedesign.app>
2025-04-21 07:47:57 +0000

    Build assets

--- codedesign-connector.php
+++ codedesign-connector.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: CodeDesign.ai Pages 
 Description: Brings the power of CodeDesign.ai 
-Version: 1.3.62
+Version: 1.3.63
 Author: CodeDesign.ai
 */
 require_once plugin_dir_path(__FILE__) . 'settings-page.php';
